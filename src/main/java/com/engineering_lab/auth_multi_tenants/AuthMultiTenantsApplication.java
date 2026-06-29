ca316ea04b0bb9316ae3f2622d3a66378cd77743
longdnh <longnh.uit@gmail.com>
2026-06-29 20:52:55 +0700

    init project

---
+++ src/main/java/com/engineering_lab/auth_multi_tenants/AuthMultiTenantsApplication.java
@@ -0,0 +1,13 @@
+package com.engineering_lab.auth_multi_tenants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMultiTenantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMultiTenantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engineering_lab/auth_multi_tenants/AuthMultiTenantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineering_lab.auth_multi_tenants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMultiTenantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

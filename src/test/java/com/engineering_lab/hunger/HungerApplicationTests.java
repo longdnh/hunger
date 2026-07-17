7cc919eeb1d46867a6fb8f4eec7176bcff9c751f
ldnh <longnh.uit@gmail.com>
2026-07-17 11:48:38 +0700

    feat: Add Testcontainers support for PostgreSQL in integration tests

--- src/test/java/com/engineering_lab/hunger/HungerApplicationTests.java
+++ src/test/java/com/engineering_lab/hunger/HungerApplicationTests.java
@@ -2,14 +2,31 @@ package com.engineering_lab.hunger;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
-@ActiveProfiles("test")
-class HungerApplicationTests {
+  @Testcontainers
+  @SpringBootTest
+  @ActiveProfiles("test")
+  class HungerApplicationTests {
+
+	private static final DockerImageName POSTGRES_IMAGE =
+			DockerImageName.parse("postgres:18");
+
+	@Container
+	@ServiceConnection
+	static final PostgreSQLContainer postgres =
+			new PostgreSQLContainer(POSTGRES_IMAGE)
+					.withDatabaseName("hunger")
+					.withUsername("postgres")
+					.withPassword("postgres");
 
 	@Test
 	void contextLoads() {
 	}
+  }
 
-}

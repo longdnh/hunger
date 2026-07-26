13930f6847ec415916308a16232b07790304a31d
Nguyen Hoang Long <longnh.uit@gmail.com>
2026-07-26 22:52:08 +0700

    feat: Add AuthenticationController for CSRF token handling

--- src/main/java/com/engineering_lab/hunger/authentication/web/AuthenticationController.java
+++ src/main/java/com/engineering_lab/hunger/authentication/web/AuthenticationController.java
@@ -1,9 +1,11 @@
-package com.engineering_lab.hunger.authentication.web.dto;
+package com.engineering_lab.hunger.authentication.web;
 
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.engineering_lab.hunger.authentication.web.dto.CsrfTokenResponse;
 
 @RestController
 @RequestMapping("/api/v1/auth")
